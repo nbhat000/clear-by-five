75d19b4dbf7e5b29e9056831fa58cc810eac7061
Nikhil Bhat <nqb5152@gmail.com>
2026-09-14 11:05:24 -0400

    Move Meta Pixel initialization inline

--- dist/script.js
+++ dist/script.js
@@ -46,16 +46,6 @@ const attribution = Object.fromEntries(
 );
 Object.entries(attribution).forEach(([key, value]) => sessionStorage.setItem(`cbf_${key}`, value));
 
-if (CONFIG.metaPixelId) {
-  !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-  n.push=n;n.loaded=!0;n.version="2.0";n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}
-  (window,document,"script","https://connect.facebook.net/en_US/fbevents.js");
-  fbq("init", CONFIG.metaPixelId);
-  fbq("track", "PageView");
-}
-
 const track = (eventName, parameters = {}) => {
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({ event: eventName, ...parameters });
